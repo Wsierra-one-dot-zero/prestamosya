484ad174b82098aa2749cb99ffe44ce52f072079
wilmersierra <wilmer.sierra.linux@gmail.com>
2025-06-25 17:02:48 -0500

    se actualiza workflow con job de desploy

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Esperar a que el servicio de credenciales de AWS esté disponible
+until aws sts get-caller-identity --region us-east-1 2>/dev/null; do
+    echo "Esperando credenciales de AWS..."
+    sleep 2
+    if [ "$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI" ]; then
+        break
+    fi
+done
+
+# Ejecutar Gunicorn
+exec gunicorn prestamosya.wsgi:application --bind 0.0.0.0:8000
